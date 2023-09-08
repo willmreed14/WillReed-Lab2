59e083fb06666f76c5f482765d5d9cfa2af91255
William Reed <wreed2020@fau.edu>
2023-09-08 18:40:53 -0400

    Tell the table view how many rows to show

--- lab-tunley/lab-tunley/TracksViewController.swift
+++ lab-tunley/lab-tunley/TracksViewController.swift
@@ -8,21 +8,24 @@
 
 import UIKit
 
-class TracksViewController: UIViewController {
+class TracksViewController: UIViewController, UITableViewDataSource {
 
     // Pt 1 - Add a tracks property
     var tracks: [Track] = [] // initially set to an empty array that will later hold 'mockTracks' array'
 
 
-    // TODO: Pt 1 - Add table view outlet
-
+    // Pt 1 - Add table view outlet
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Pt 1 - Set tracks property with mock tracks array
         tracks = Track.mockTracks
         print(tracks)
-
+        
+        // Set tableview source to itself
+        tableView.dataSource = self
         
     }
 
@@ -32,7 +35,16 @@ class TracksViewController: UIViewController {
 
     }
 
-    // TODO: Pt 1 - Add table view data source methods
+    // Pt 1 - Add table view data source methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // tell the table view how many rows to show
+        return tracks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
 
 }
